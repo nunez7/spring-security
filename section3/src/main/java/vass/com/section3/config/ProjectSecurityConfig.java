64f798c726f7900fb4b2fd8b932d2d928d1f50ca
Felix Nunez <felix.nunez@vass.com.mx>
2025-01-08 16:38:53 -0700

    Definiendo usuarios en memoria

---
+++ section3/src/main/java/vass/com/section3/config/ProjectSecurityConfig.java
@@ -0,0 +1,58 @@
+package vass.com.section3.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> requests
+                .requestMatchers("/account", "/balance", "/loans", "/cards").authenticated()
+                .requestMatchers("/notices", "/contact", "/error").permitAll());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user").password("{noop}EazyBytes@12345").authorities("read").build();
+        UserDetails admin = User.withUsername("admin")
+                .password("{bcrypt}$2a$12$d8ZGzWkWfqYFgXqyKtVwQ.l0ZTOXQoN7LYREuA17FVkuSvUOzf96a")
+                .authorities("admin").build();
+        /*
+         * Password generate from https://bcrypt-generator.com/ EazyBytes@12348
+         * @return
+         */
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    /**
+     * From Spring Security 6.3 version
+     * Ayuda a verificar si la contrasenia es robusta o no
+     * @return
+     */
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+}
---
+++ section3/src/test/java/vass/com/section3/Section1ApplicationTests.java
@@ -0,0 +1,13 @@
+package vass.com.section3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
