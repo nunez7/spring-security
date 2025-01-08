a3d63b77021dd47fda4019979609103248840c56
Felix Nunez <felix.nunez@vass.com.mx>
2025-01-08 15:42:48 -0700

    Deshabilitando formLogin y httpBasicAuth

--- section2/src/main/java/vass/com/section2/config/ProjectSecurityConfig.java
+++ section2/src/main/java/vass/com/section2/config/ProjectSecurityConfig.java
@@ -3,6 +3,7 @@ package vass.com.section2.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -19,6 +20,8 @@ public class ProjectSecurityConfig {
                 .requestMatchers("/notices", "/contact", "/error").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
+        //http.formLogin(AbstractHttpConfigurer::disable);
+        //http.httpBasic(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
