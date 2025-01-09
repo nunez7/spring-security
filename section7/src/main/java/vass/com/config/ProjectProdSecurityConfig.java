a23d8dd8bfd14d14c358c26b42e94ccbae10e71e
Felix Nunez <felix.nunez@vass.com.mx>
2025-01-09 12:15:35 -0700

    AccessDeniedHandler agregado

--- section7/src/main/java/vass/com/config/ProjectProdSecurityConfig.java
+++ section7/src/main/java/vass/com/config/ProjectProdSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+import vass.com.exceptionhandling.CustomAccessDeniedHandler;
 import vass.com.exceptionhandling.CustomBasicAuthenticationEntryPoint;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -27,6 +28,7 @@ public class ProjectProdSecurityConfig {
                 .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
+        http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
         return http.build();
     }
 
--- section7/src/main/java/vass/com/config/ProjectSecurityConfig.java
+++ section7/src/main/java/vass/com/config/ProjectSecurityConfig.java
@@ -9,8 +9,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import vass.com.exceptionhandling.CustomBasicAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+import vass.com.exceptionhandling.CustomAccessDeniedHandler;
+import vass.com.exceptionhandling.CustomBasicAuthenticationEntryPoint;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -27,6 +28,7 @@ public class ProjectSecurityConfig {
                 .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
+        http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
         return http.build();
     }
 
