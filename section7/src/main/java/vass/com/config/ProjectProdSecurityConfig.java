26cfc452fae22c35270e3f83be6249fc1107bd52
Felix Nunez <felix.nunez@vass.com.mx>
2025-01-09 11:33:07 -0700

    Spring Security Customizations
    - En clases ProjectSecurityConfig  y ProjectProdSecurityConfig se configura para que solo acepte peticioens HTTPS o HTTP

---
+++ section7/src/main/java/vass/com/config/ProjectProdSecurityConfig.java
@@ -0,0 +1,51 @@
+package vass.com.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@Profile("prod")
+public class ProjectProdSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.requiresChannel(rcc -> rcc.anyRequest().requiresSecure()) // Only HTTPS
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests -> requests
+                .requestMatchers("/account", "/balance", "/loans", "/cards").authenticated()
+                .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    /*@Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }*/
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    /**
+     * From Spring Security 6.3 version
+     * Ayuda a verificar si la contrasenia es robusta o no
+     * @return
+     */
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+}
---
+++ section7/src/main/java/vass/com/controller/CardsController.java
@@ -0,0 +1,14 @@
+package vass.com.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CardsController {
+
+    @GetMapping("/cards")
+    public String getCardsDetails(){
+        return "Here are the cards details from the DB";
+    }
+
+}
