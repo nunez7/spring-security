9b009acc904bc287feb16aef0308d11b57f3fb98
Felix Nunez <felix.nunez@vass.com.mx>
2025-01-09 11:11:04 -0700

    Prod profiles in spring boot

--- section6/src/main/java/vass/com/config/EazyBankProdUsernamePwdAuthenticationProvider.java
+++ section6/src/main/java/vass/com/config/EazyBankProdUsernamePwdAuthenticationProvider.java
@@ -3,6 +3,7 @@ package vass.com.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -12,10 +13,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("!prod")
+@Profile("prod")
 @RequiredArgsConstructor
-public class EazyBankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
-
+public class EazyBankProdUsernamePwdAuthenticationProvider implements AuthenticationProvider {
     private final UserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
 
@@ -24,7 +24,12 @@ public class EazyBankUsernamePwdAuthenticationProvider implements Authentication
         String username = authentication.getName();
         String pwd = authentication.getCredentials().toString();
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-        return new UsernamePasswordAuthenticationToken(username,pwd,userDetails.getAuthorities());
+        if (passwordEncoder.matches(pwd, userDetails.getPassword())) {
+            // Fetch Age details and perform validation to check if age >18
+            return new UsernamePasswordAuthenticationToken(username,pwd,userDetails.getAuthorities());
+        }else {
+            throw new BadCredentialsException("Invalid password!");
+        }
     }
 
     @Override
