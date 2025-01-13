cbf51c0eada97a9d5ecca26a1dc9db27767aa1f5
Felix Nunez <felix.nunez@vass.com.mx>
2025-01-13 12:10:07 -0700

    Prefilter en backend

--- section12/section12/src/main/java/vass/com/controller/ContactController.java
+++ section12/section12/src/main/java/vass/com/controller/ContactController.java
@@ -1,6 +1,7 @@
 package vass.com.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,8 @@ import vass.com.model.Contact;
 import vass.com.service.contract.ContactService;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @RestController
@@ -18,10 +21,17 @@ public class ContactController {
     private final Random random = new Random();
 
     @PostMapping("/contact")
-    public Contact saveContactInquiryDetails(@RequestBody Contact contact) {
-        contact.setContactId(getServiceReqNumber());
-        contact.setCreateDt(new Date(System.currentTimeMillis()));
-        return service.save(contact);
+    @PostFilter("filterObject.contactName != 'Test'")
+    public List<Contact> saveContactInquiryDetails(@RequestBody List<Contact> contacts) {
+        List<Contact> returnContacts = new ArrayList<>();
+        if(!contacts.isEmpty()) {
+            Contact contact = contacts.get(0);
+            contact.setContactId(getServiceReqNumber());
+            contact.setCreateDt(new Date(System.currentTimeMillis()));
+            Contact savedContact = service.save(contact);
+            returnContacts.add(savedContact);
+        }
+        return returnContacts;
     }
 
     public String getServiceReqNumber() {
