3d48891ae5fa5be1c87da8396cf0968bf8a3e369
Felix Nunez <felix.nunez@vass.com.mx>
2025-01-13 11:50:17 -0700

    Working con Method Level Security
    Se mueve a folder el backend

--- section12/section12/src/main/java/vass/com/MainApplication.java
+++ section12/section12/src/main/java/vass/com/MainApplication.java
@@ -2,7 +2,9 @@ package vass.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
+@EnableMethodSecurity(securedEnabled = true)
 @SpringBootApplication
 public class 	MainApplication {
 
